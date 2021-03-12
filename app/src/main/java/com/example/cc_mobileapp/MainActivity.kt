b3d54fdc97e4724221964b113151da035f1108ee
YChuaC0621 <67249007+YChuaC0621@users.noreply.github.com>
2021-03-12 19:28:37 +0800

    Update

--- app/src/main/java/com/example/cc_mobileapp/MainActivity.kt
+++ app/src/main/java/com/example/cc_mobileapp/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.cc_mobileapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mongodb.MongoClient
+import com.mongodb.MongoClientURI
+
+
+var uri = MongoClientURI("mongodb+srv://CYC:cyc1234@cluster0.zyrbf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+var mongoClient = MongoClient(uri)
+var db = mongoClient.getDatabase(uri.database)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
