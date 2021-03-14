2d2bad4d2b74edb8598a097a5acbc63b232d5319
Suying1020 <cheahsy-pm18@student.tarc.edu.my>
2021-03-14 21:32:23 +0800

    Add firebase

--- app/src/main/java/com/example/cc_mobileapp/MainActivity.kt
+++ app/src/main/java/com/example/cc_mobileapp/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.example.cc_mobileapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.mongodb.MongoClient
 import com.mongodb.MongoClientURI
 
 
-var uri = MongoClientURI("mongodb+srv://CYC:cyc1234@cluster0.zyrbf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-var mongoClient = MongoClient(uri)
-var db = mongoClient.getDatabase(uri.database)
+//var uri = MongoClientURI("mongodb+srv://CYC:cyc1234@cluster0.zyrbf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+//var mongoClient = MongoClient(uri)
+//var db = mongoClient.getDatabase(uri.database)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "firebase connection success", Toast.LENGTH_LONG ).show()
     }
 }
