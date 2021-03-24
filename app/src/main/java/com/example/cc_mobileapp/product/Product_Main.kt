0882b2be626e81e67be251ee40c8448cda645d3e
Suying1020 <cheahsy-pm18@student.tarc.edu.my>
2021-03-24 20:36:42 +0800

    Update misplace Supplier Item info

---
+++ app/src/main/java/com/example/cc_mobileapp/product/Product_Main.kt
@@ -0,0 +1,12 @@
+package com.example.cc_mobileapp.product
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cc_mobileapp.R
+
+class Product_Main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product__main)
+    }
+}
